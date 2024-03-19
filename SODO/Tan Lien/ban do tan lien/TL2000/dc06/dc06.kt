0834c330b0e15c88cbd82ff2b3eae0be4712cf47
guitarkhongem <guitarkhongem@gmail.com>
2024-03-19 12:49:25 +0700

    s

---
+++ SODO/Tan Lien/ban do tan lien/TL2000/dc06/dc06.kt
@@ -0,0 +1,30 @@
+        KiÓm tra th«ng tin trong file txt vµ nh·n thöa
+
+        Tæng sè thöa lín h¬n tæng sè nh·n:   17  thöa
+
+------------------------------------------------------
+KiÓm tra file txt so víi nh·n thöa
+
+ 1  Thöa  790  Kh«ng cã nh·n
+ 2  Thöa  791  Kh«ng cã nh·n
+ 3  Thöa  792  Kh«ng cã nh·n
+ 4  Thöa  793  Kh«ng cã nh·n
+ 5  Thöa  794  Kh«ng cã nh·n
+ 6  Thöa  795  Kh«ng cã nh·n
+ 7  Thöa  796  Kh«ng cã nh·n
+ 8  Thöa  797  Kh«ng cã nh·n
+ 9  Thöa  798  Kh«ng cã nh·n
+ 10  Thöa  799  Kh«ng cã nh·n
+ 11  Thöa  800  Kh«ng cã nh·n
+ 12  Thöa  801  Kh«ng cã nh·n
+ 13  Thöa  802  Kh«ng cã nh·n
+ 14  Thöa  803  Kh«ng cã nh·n
+ 15  Thöa  804  Kh«ng cã nh·n
+ 16  Thöa  805  Kh«ng cã nh·n
+ 17  Thöa  806  Kh«ng cã nh·n
+ 18  Thöa  807  Kh«ng cã nh·n
+
+------------------------------------------------------
+KiÓm tra nh·n thöa so víi file txt
+
+Chóc mõng b¹n kh«ng cã lçi nµy
---
+++ SODO/Tan Lien/ban do tan lien/TL2000/dc15/dc15.kt
@@ -0,0 +1,18 @@
+        KiÓm tra th«ng tin trong file txt vµ nh·n thöa
+
+        Tæng sè thöa lín h¬n tæng sè nh·n:   6  thöa
+
+------------------------------------------------------
+KiÓm tra file txt so víi nh·n thöa
+
+ 1  Thöa  145  Kh«ng cã nh·n
+ 2  Thöa  146  Kh«ng cã nh·n
+ 3  Thöa  147  Kh«ng cã nh·n
+ 4  Thöa  148  Kh«ng cã nh·n
+ 5  Thöa  149  Kh«ng cã nh·n
+ 6  Thöa  150  Kh«ng cã nh·n
+
+------------------------------------------------------
+KiÓm tra nh·n thöa so víi file txt
+
+Chóc mõng b¹n kh«ng cã lçi nµy
